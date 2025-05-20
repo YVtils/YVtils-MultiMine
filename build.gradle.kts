76420cfe5d2023e8105985c6623cd45e6f78c9fc
WolfiiYV <minecraftwandler@gmail.com>
2025-05-20 15:25:08 +0200

    Update commandapi (10.0.0 -> 10.0.1) and other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,15 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.1.0"
-    kotlin("plugin.serialization") version "2.1.0"
-
+    kotlin("jvm") version "2.1.21"
+    kotlin("plugin.serialization") version "2.1.21"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-
-    id("io.papermc.paperweight.userdev") version "1.7.6"
-
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.16"
     id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
-val yvtilsVersion = "1.1.0"
-val commandAPIVersion = "9.7.0"
+val yvtilsVersion = "1.1.2"
+val commandAPIVersion = "10.0.1"
 
 group = "yv.tils"
 version = yvtilsVersion
